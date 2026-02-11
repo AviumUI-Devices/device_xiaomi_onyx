3f0f0939001ca07cfe192543f1299baa669719f2
Zarathos30 <vale.rigni@gmail.com>
2026-02-11 22:49:17 +0100

    onyx: Bringup Avium
    Change-Id: I0c9832ebfffa07ac52f083c10b3561998e8d7270

--- lineage_onyx.mk
+++ lineage_onyx.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Include Avium common configuration
+include device/xiaomi/onyx/avium_common.mk
+
 # Inherit from onyx device
 $(call inherit-product, device/xiaomi/onyx/device.mk)
 
